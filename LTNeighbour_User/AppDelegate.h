e123df5b07c71e96654841976324702743773225
junchen <junchen@junchendeMacBook-Pro.local>
2016-05-31 12:17:18 +0800

    second commit

---
+++ LTNeighbour_User/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LTNeighbour_User
+//
+//  Created by baoju_mac_2 on 16/3/14.
+//  Copyright © 2016年 baoju_mac_2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
++(AppDelegate*)shareInstance;
+@end
+
